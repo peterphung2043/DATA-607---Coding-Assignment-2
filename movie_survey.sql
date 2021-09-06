d83c1b9616a20dc85786f9e8d5e539885ddf4930
peterphung2043 <59744683+peterphung2043@users.noreply.github.com>
2021-09-05 21:24:10 -0400

    Add files via upload

---
+++ movie_survey.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS movie_survey;
+
+CREATE TABLE movie_survey (
+	`Survey ID` INT,
+    `Joker` INT,
+    `Whiplash` INT,
+    `Us` INT,
+    `Once Upon a Time in Hollywood` INT,
+    `The Rise of Skywalker` INT,
+    PRIMARY KEY (`Survey ID`)
+);
+
+
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (0,5,4,2,1,3);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (1,NULL,4,NULL,3,3);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (2,5,1,NULL,1,1);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (3,1,5,NULL,2,5);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (4,4,4,5,1,3);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (5,1,NULL,5,1,1);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (6,3,2,2,NULL,5);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (7,3,5,4,2,3);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (8,4,5,2,1,5);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (9,NULL,NULL,5,2,NULL);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (10,5,4,1,3,1);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (11,1,NULL,3,2,2);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (12,4,4,4,2,1);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (13,NULL,4,5,4,1);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (14,1,4,NULL,2,3);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (15,1,3,4,4,3);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (16,4,NULL,1,3,4);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (17,5,1,5,5,2);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (18,NULL,3,3,1,4);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (19,3,5,2,4,2);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (20,NULL,NULL,4,NULL,NULL);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (21,4,4,5,5,3);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (22,5,3,3,1,1);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (23,2,3,5,NULL,3);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (24,NULL,1,2,5,2);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (25,4,4,NULL,5,5);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (26,4,NULL,5,3,5);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (27,4,1,NULL,5,3);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (28,2,2,2,2,4);
+INSERT INTO movie_survey(`Survey ID`,`Joker`,`Whiplash`,`Us`,`Once Upon a Time in Hollywood`,`The Rise of Skywalker`) VALUES (29,NULL,NULL,1,NULL,2);
+
+SELECT * FROM movie_survey;
+
